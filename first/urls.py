342487328862535b5cafde0d6bbeef6d901c8dc8
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2022-04-12 19:46:53 +0530

    First Commit

---
+++ first/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("expense/", include("expense.urls", namespace="expenses")),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+]
